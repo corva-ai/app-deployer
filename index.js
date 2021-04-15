82a89206e8f7a5ceab988ee6f40d7846a179afa8
Jordan Ambra <jordan@serenity.software>
2021-04-15 16:53:53 -0400

    Even better for loop async for blacklisting

--- index.js
+++ index.js
@@ -73,11 +73,11 @@ async function generatePackageFile() {
 
     core.info('Removing blacklisted directories');
     const blacklist = ['.git', '.github', '.gitignore'];
-    blacklist.forEach(async (blacklistPath) => {
+    for (blacklistPath of blacklist) {
         blacklistPath = path.join(tempStorage, blacklistPath);
         core.info(`Removing ${blacklistPath}`);
         await io.rmRF(blacklistPath);
-    });
+    }
 
     core.info('Archiving files to zip')
     const packagePath = path.join(os.tmpdir(), 'package.zip');
